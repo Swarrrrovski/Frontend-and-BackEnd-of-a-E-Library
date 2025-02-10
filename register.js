9dc618a74a03ef9a1b3ddec73e5a507348017aa5
Swarrrrovski <thrillophile678@gmail.com>
2025-02-10 15:00:38 +0530

    plp

--- register.js
+++ register.js
@@ -1,72 +1,47 @@
-function selectRole(role) {
-    document.querySelector('.role-selection').style.display = 'none';
-    document.querySelector('.registration-container').style.display = 'block';
-    document.getElementById('selectedRoleText').innerText = "Registering as: " + role.toUpperCase();
-}
-
 function validateForm(event) {
     event.preventDefault(); 
-
     var pw1 = document.getElementById("password").value;
     var pw2 = document.getElementById("confirmPassword").value;
-    var name1 = document.getElementById("fname").value;
-    var name2 = document.getElementById("lname").value;
+    var fname = document.getElementById("fname").value;
+    var lname = document.getElementById("lname").value;
     var username = document.getElementById("username").value;
 
-    document.getElementById("blankMsg").innerHTML = "";
-    document.getElementById("charMsg").innerHTML = "";
-    document.getElementById("message1").innerHTML = "";
-    document.getElementById("message2").innerHTML = "";
+    
+    document.getElementById("error-message").innerHTML = "";
 
-    if (name1 === "") {
-        document.getElementById("blankMsg").innerHTML = "**Fill the first name";
+   
+    if (fname === "" || lname === "" || username === "" || pw1 === "" || pw2 === "") {
+        document.getElementById("error-message").innerHTML = "All fields are required.";
         return false;
     }
 
-    if (!isNaN(name1)) {
-        document.getElementById("blankMsg").innerHTML = "**Only characters are allowed";
-        return false;
-    }
-
-    if (!isNaN(name2)) {
-        document.getElementById("charMsg").innerHTML = "**Only characters are allowed";
-        return false;
-    }
-
-    if (pw1 === "") {
-        document.getElementById("message1").innerHTML = "**Fill the password please!";
-        return false;
-    }
-
-    if (pw2 === "") {
-        document.getElementById("message2").innerHTML = "**Enter the password please!";
-        return false;
-    }
-
-    if (pw1.length < 8) {
-        document.getElementById("message1").innerHTML = "**Password length must be at least 8 characters";
-        return false;
-    }
-
-    if (pw1.length > 15) {
-        document.getElementById("message1").innerHTML = "**Password length must not exceed 15 characters";
-        return false;
-    }
-
+    
     if (pw1 !== pw2) {
-        document.getElementById("message2").innerHTML = "**Passwords are not the same";
+        document.getElementById("error-message").innerHTML = "Passwords do not match.";
         return false;
     }
 
+    
+    if (pw1.length < 8) {
+        document.getElementById("error-message").innerHTML = "Password must be at least 8 characters long.";
+        return false;
+    }
+
+    
     localStorage.setItem("username", username);
     localStorage.setItem("password", pw1);
 
-    alert("Registration successful!");
+    
+    var role = document.getElementById("selectedRoleText").innerText.split(": ")[1].toLowerCase();
 
     
-    window.location.href = "user_login.html";
-    return true;
-}
-window.location.href = "user_login.html";
+    if (role === 'Admin') {
+        document.location.href = "admin_login.html";
 
-document.getElementById('registrationForm').addEventListener('submit', validateForm);
+    } else if (role === 'User') {
+        document.location.href = "user_login.html";
+
+    } else {
+        alert("Invalid role selection.");
+    }
+}
