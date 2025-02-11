005f9468bfed23827f411f699c23669d8ce8e781
Swarrrrovski <thrillophile678@gmail.com>
2025-02-11 09:36:59 +0530

    p

--- Admin/admin_login.php
+++ Admin/admin_login.php
@@ -1,5 +1,5 @@
 <?php
-include "connection.php";
+include "connection.php"; // Ensure the connection is correct
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -38,31 +38,36 @@ include "connection.php";
     <script src="admin_login.js"></script>
     
     <?php
-    if(isset($_POST['login']))
-    {
-        
+    if (isset($_POST['login'])) {
+        // Sanitize inputs to avoid SQL injection
         $username = mysqli_real_escape_string($db, $_POST['username']);
         $password = mysqli_real_escape_string($db, $_POST['password']);
-        $count = 0;
-        $res=mysqli_query($db,"SELECT * FROM `student` WHERE username='$_POST[username]' && password='$_POST[password]';");
-        $count = mysqli_num_rows($res);
-        if($count > 0)
-        {
-            
-            ?>
-            <script type="text/javascript">
-                window.location.href = "ind.php";
-            </script>
-            <?php
-        }
-        else
-        {
-            
-            ?>
-            <div class="alert alert-danger" style="width: 600px; margin-left: 370px; background-color: #de1313; color: white">
-                <strong>The username and password don't match</strong>
-            </div>    
-            <?php
+        
+        // Query to check username and password
+        $query = "SELECT * FROM `admin_register` WHERE username='$username' AND password='$password'";
+        $res = mysqli_query($db, $query);
+        
+        // Check if query ran successfully
+        if ($res === false) {
+            // Display query error (for development purposes, should be logged instead in production)
+            echo "Query error: " . mysqli_error($db);
+        } else {
+            $count = mysqli_num_rows($res);
+            if ($count > 0) {
+                // If login is successful, redirect to ind.php
+                ?>
+                <script type="text/javascript">
+                    window.location.href = "ind.php";
+                </script>
+                <?php
+            } else {
+                // If login fails, display error message
+                ?>
+                <div class="alert alert-danger" style="width: 600px; margin-left: 370px; background-color: #de1313; color: white">
+                    <strong>The username and password don't match</strong>
+                </div>
+                <?php
+            }
         }
     }
     ?>
