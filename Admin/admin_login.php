db6ef18bd6445a772171f6f546303dadba9640c5
Swarrrrovski <thrillophile678@gmail.com>
2025-02-11 09:19:01 +0530

    l

--- Admin/admin_login.php
+++ Admin/admin_login.php
@@ -6,7 +6,7 @@ include "connection.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>vault - Login</title>
+    <title>Vault - Login</title>
     <link rel="stylesheet" href="admin_login.css">
     <link rel="shortcut icon" href="book.png" type="image/x-icon">
 </head>
@@ -18,8 +18,7 @@ include "connection.php";
                 <label for="role">Login as:</label>
                 <select id="role" name="role" required>
                     <option value="" disabled selected>Select Role</option>
-                    <option value="user">Admin</option>
-                    
+                    <option value="admin">Admin</option>
                 </select>
             </div>
             <div class="form-group">
@@ -37,35 +36,35 @@ include "connection.php";
         <p>Not registered yet? <a href="admin_register.php">Register Now</a></p>
     </div>
     <script src="admin_login.js"></script>
+    
     <?php
-
     if(isset($_POST['login']))
     {
-      $count=0;
-      $res=mysqli_query($db,"SELECT * FROM `admin_register` WHERE username='$_POST[username]' && password='$_POST[password]';");
-      
-      $row= mysqli_fetch_assoc($res);
-      $count=mysqli_num_rows($res);
-
-      if($count==0)
-      {
-        ?>
-             
-          <div class="alert alert-danger" style="width: 600px; margin-left: 370px; background-color: #de1313; color: white">
-            <strong>The username and password doesn't match</strong>
-          </div>    
-        <?php
-      }
-      else
-      {
-        ?>
-          <script type="text/javascript">
-            window.location.href="ind.php"
-          </script>
-        <?php
-      }
+        
+        $username = mysqli_real_escape_string($db, $_POST['username']);
+        $password = mysqli_real_escape_string($db, $_POST['password']);
+        $count = 0;
+        $res=mysqli_query($db,"SELECT * FROM `student` WHERE username='$_POST[username]' && password='$_POST[password]';");
+        $count = mysqli_num_rows($res);
+        if($count > 0)
+        {
+            
+            ?>
+            <script type="text/javascript">
+                window.location.href = "ind.php";
+            </script>
+            <?php
+        }
+        else
+        {
+            
+            ?>
+            <div class="alert alert-danger" style="width: 600px; margin-left: 370px; background-color: #de1313; color: white">
+                <strong>The username and password don't match</strong>
+            </div>    
+            <?php
+        }
     }
-
-  ?>
+    ?>
 </body>
 </html>
