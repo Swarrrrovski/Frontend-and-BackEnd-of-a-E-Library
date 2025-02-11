9af077ac00dedd82f6e3f7f396e52138d65726d8
Swarrrrovski <thrillophile678@gmail.com>
2025-02-11 08:34:30 +0530

    p

--- Admin/admin_login.php
+++ Admin/admin_login.php
@@ -1,3 +1,6 @@
+<?php
+include "connection.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,7 +13,7 @@
 <body>
     <div class="login-container">
         <h1>Vault Admin</h1>
-        <form id="loginForm">
+        <form id="loginForm" action="" method="post">
             <div class="form-group">
                 <label for="role">Login as:</label>
                 <select id="role" name="role" required>
@@ -28,11 +31,44 @@
                 <input type="password" id="password" name="password" placeholder="Enter Password" required>
             </div>
             <button type="submit">Login</button>
+            <button type="submit" name="login">Login</button>
         </form>
         <p id="error-message" class="error-message"></p>
         <p>Not registered yet? <a href="admin_register.php">Register Now</a></p>
     </div>
     <script src="admin_login.js"></script>
-    
+    <?php
+
+    if(isset($_POST['login']))
+    {
+      $count=0;
+      $res=mysqli_query($db,"SELECT * FROM `admin_register` WHERE username='$_POST[username]' && password='$_POST[password]';");
+      
+      $row= mysqli_fetch_assoc($res);
+      $count=mysqli_num_rows($res);
+
+      if($count==0)
+      {
+        ?>
+             
+          <div class="alert alert-danger" style="width: 600px; margin-left: 370px; background-color: #de1313; color: white">
+            <strong>The username and password doesn't match</strong>
+          </div>    
+        <?php
+      }
+      else
+      {
+        $_SESSION['login_user'] = $_POST['username'];
+        $_SESSION['pic']= $row['pic'];
+
+        ?>
+          <script type="text/javascript">
+            window.location ="admin_dashboard.php"
+          </script>
+        <?php
+      }
+    }
+
+  ?>
 </body>
 </html>
