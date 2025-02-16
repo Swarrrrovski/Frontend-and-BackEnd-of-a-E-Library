616f24db38cbdd06f25723444a111cbd34395840
Swarrrrovski <thrillophile678@gmail.com>
2025-02-16 20:52:42 +0530

    p

--- Admin/connection.php
+++ Admin/connection.php
@@ -1,6 +1,6 @@
 <?php
 
-	$db=mysqli_connect("localhost","root","","vault_library_management");  
+	$db =mysqli_connect("localhost","root","","vault_library_management");  
     
     if (!$db)
     {
